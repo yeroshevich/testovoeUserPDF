44bc635e3d193f2c91894251ec398a515c72ad65
yeroshevich valeriy <pybin2001@gmail.com>
2023-05-06 13:06:45 +0300

    feat: refact entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,8 @@ if ([ ! -f "/ssl_cert/key.pem" ] && [ ! -f "/ssl_cert/cert.pem" ]); then
          && openssl req -new -x509 -nodes -sha256 -days 9999 \
              -key /ssl_cert/key.pem -out /ssl_cert/cert.pem \
              -subj '/CN=localhost'
-
+     #chown node:node /ssl_cert/key.pem
+     #chown node:node /ssl_cert/cert.pem
 fi
 
 mkdir /root/.ssh
@@ -24,16 +25,19 @@ chmod 600 /root/.ssh/*
 echo '
 '
 echo '🧩🧩🧩🧩🧩 Begin install 🧩🧩🧩🧩🧩'
-yarn install
+# npm install
+yarn
 
 echo '
 '
+# echo '🧩🧩🧩🧩🧩 Begin audit fix 🧩🧩🧩🧩🧩'
+# npm audit fix
+
+
 chown node:node -Rf /var/www/.logs
 chown node:node -Rf /var/www/node_modules
 
-
 yarn prisma migrate dev --name init
-
 
 echo '
 '
